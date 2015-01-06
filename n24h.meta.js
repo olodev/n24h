80e9e6e52ed13a1faeada65844edafe6d761a792
olodev <olodev@wp.pl>
2015-01-06 13:04:15 +0100

    Update n24h.meta.js

--- n24h.meta.js
+++ n24h.meta.js
@@ -5,6 +5,8 @@
 // @description     Dodaje kilka ciekawych funkcji na stronie napisy24.pl
 // @namespace       KO/napisy24_helper
 // @include         http://napisy24.pl/*
+// @downloadURL     https://github.com/olodev/n24h/raw/master/n24h.user.js
+// @updateURL       https://github.com/olodev/n24h/raw/master/n24h.meta.js
 // @grant           none
 // @run-at          document-start
 // ==/UserScript==
