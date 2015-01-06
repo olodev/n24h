c45044a228baf1c0a5ba8e7ab38d23485b965a0f
olodev <olodev@wp.pl>
2015-01-06 13:00:58 +0100

    Create n24h.meta.js

---
+++ n24h.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name            napisy24.pl helper
+// @version         0.8 beta
+// @author          KO
+// @description     Dodaje kilka ciekawych funkcji na stronie napisy24.pl
+// @namespace       KO/napisy24_helper
+// @include         http://napisy24.pl/*
+// @grant           none
+// @run-at          document-start
+// ==/UserScript==
